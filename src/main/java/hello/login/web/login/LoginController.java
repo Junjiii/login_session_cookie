dce059d4a10fcfbb6d4fd00e9ad15d07fc5307af
Jjunjiii’ <drum1701@gmail.com>
2024-11-06 14:38:15 +0900

    Login : LoginFilter

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -17,6 +17,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -70,9 +71,34 @@ public class LoginController {
 //
 //        return "redirect:/";
 //    }
-
+//
+//    @PostMapping("/login")
+//    public String loginV3(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+//        if(bindingResult.hasErrors()) {
+//            return "login/loginForm";
+//        }
+//
+//        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+//
+//        if(loginMember == null) {
+//            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
+//            return "login/loginForm";
+//        }
+//
+//        // 로그인 성공 처리
+//        // 기존 세션이 있으면 재사용
+//        HttpSession session = request.getSession();
+//
+//        // 세션에 회원 정보 보관
+//        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+//
+//        return "redirect:/";
+//    }
+//
     @PostMapping("/login")
-    public String loginV3(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+    public String loginV4(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/",name = "redirectURL") String redirectURL,
+                          HttpServletRequest request) {
         if(bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -91,7 +117,7 @@ public class LoginController {
         // 세션에 회원 정보 보관
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
-        return "redirect:/";
+        return "redirect:" + redirectURL;
     }
 
 //    @PostMapping("logout")
